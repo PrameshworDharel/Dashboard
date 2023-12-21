67e929522948212cb2cc87b345112abe18a7e3a4
Prameshwor <dharelprameshwor@gmail.com>
2023-12-21 11:50:24 +0545

    dashboard

--- src/Navbar/topbar.jsx
+++ src/Navbar/topbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { LiaToggleOnSolid } from "react-icons/lia";
 import { IoNotificationsCircle } from "react-icons/io5";
@@ -6,6 +6,10 @@ import { AiOutlineMessage } from "react-icons/ai";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { SlArrowDown } from "react-icons/sl";
 const TopBar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const handleDropdownClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
     <>
       <div className="bg-Clay p-5 ml-[275px] text-primary flex justify-between -translate-y-[750px] ">
@@ -38,9 +42,29 @@ const TopBar = () => {
           <button>
             <IoPersonCircleSharp className="w-20 h-20" />
           </button>
-          <button>
-            <SlArrowDown className="w-5 h-5" />
-          </button>
+          <div className="relative mt-7">
+            <button>
+              <SlArrowDown className="w-5 h-5 " onClick={handleDropdownClick} />
+            </button>
+            {isDropdownOpen && (
+              <div className="bg-primary text-Mirage p-10 font-bold absolute right-0 w-60 mt-[54px] shadow-xm ">
+                <button className="hover:text-WildBlue mt-5 ">
+                  My Profile
+                </button>
+                <br></br>
+                <button className="hover:text-WildBlue mt-5">
+                  My Contects
+                </button>
+                <br></br>
+                <button className="hover:text-WildBlue mt-5">
+                  Account Setting
+                </button>
+                <br></br>
+                <hr className="hover:text-WildBlue mt-5"></hr>
+                <button className="hover:text-WildBlue mt-5">Log out</button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
